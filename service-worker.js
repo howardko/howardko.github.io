20660565894c169071dcce86eba8a457dbe3af51
Hao-wenKo <hao-wenko@Hao-wenKodeMacBook-Pro.local>
2015-11-30 16:53:52 +0800

    add fetch

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,47 @@
 self.addEventListener('push', function(event) {  
-  console.log('Received a push message', event);
-
-  var title = 'Yay a message.';  
-  var body = 'We have received a push message.';  
-  var icon = '/images/icon-192x192.png';  
-  var tag = 'simple-push-demo-notification-tag';
-
+  // Since there is no payload data with the first version  
+  // of push messages, we'll grab some data from  
+  // an API and use it to populate a notification  
   event.waitUntil(  
-    self.registration.showNotification(title, {  
-      body: body,  
-      icon: icon,  
-      tag: tag  
+    fetch("http://localhost:8080/v1.1/monitor/event/eB0IKOxeJyA:APA91bGG-IX4ybHVtTegodsAQIQUEWXXFDEYnWrHYwUrUJ4hm9o0H-Jb0SRexROKVcYrD2rOk7dtZ6vDgobcx80CN9eAGJdyw-Gm3S402s-0qve_x6uLvn4VxXDXPwpflSdcEO5HLDUW").then(function(response) {  
+      if (response.status !== 200) {  
+        // Either show a message to the user explaining the error  
+        // or enter a generic message and handle the
+        // onnotificationclick event to direct the user to a web page  
+        console.log('Looks like there was a problem. Status Code: ' + response.status);  
+        throw new Error();  
+      }
+
+      // Examine the text in the response  
+      return response.json().then(function(data) {  
+        if (data.error || !data.result) {  
+          console.error('The API returned an error.', data.error);  
+          throw new Error();  
+        }  
+
+        var title = "Qnap message";  
+        var message = data.result.message;  
+        var icon = data.notification.icon;  
+        var tag = 'simple-push-demo-notification-tag';
+
+        return self.registration.showNotification(title, {  
+          body: message,  
+          icon: icon,  
+          tag: tag  
+        });  
+      });  
+    }).catch(function(err) {  
+      console.error('Unable to retrieve data', err);
+
+      var title = 'An error occurred';
+      var message = 'We were unable to get the information for this push message';  
+      var icon = URL_TO_DEFAULT_ICON;  
+      var notificationTag = 'notification-error';  
+      return self.registration.showNotification(title, {  
+          body: message,  
+          icon: icon,  
+          tag: notificationTag  
+        });  
     })  
   );  
 });
